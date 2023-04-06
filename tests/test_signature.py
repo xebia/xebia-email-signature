cdc9eb7e472021af1fda60310edf41c193513133
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2023-04-06 21:00:08 +0200

    fix: remove test for deleted signature

--- tests/test_signature.py
+++ tests/test_signature.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from xebia_email_signature.signature import (
     render_template,
     validate_html,
-    add_office_details,
+    add_weekday_availability,
 )
 
 
@@ -17,14 +17,8 @@ class Test(TestCase):
             "unit": "Xebia Cloud",
             "github_url": "https://github.com/user",
         }
-        self.employee_details = add_office_details(self.employee_details)
+        self.employee_details = add_weekday_availability(self.employee_details)
 
     def test_official_template(self):
         rendered_output = render_template(self.employee_details, "signature.html.jinja")
-        _ = validate_html(rendered_output)
-
-    def test_unofficial_template(self):
-        rendered_output = render_template(
-            self.employee_details, "simple-signature.html.jinja"
-        )
         _ = validate_html(rendered_output)
